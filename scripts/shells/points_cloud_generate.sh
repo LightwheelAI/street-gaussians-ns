f298b43582a5f15a9143bf7c50ab43d5cb6d4b9f
LightwheelAI <github@lightwheel.net>
2024-05-24 11:00:38 +0800

    FIX pcd2colmap bash command

--- scripts/shells/points_cloud_generate.sh
+++ scripts/shells/points_cloud_generate.sh
@@ -4,10 +4,9 @@ mkdir $root/colmap/sparse/lidar
 
 python scripts/pythons/pcd2colmap_points3D.py \
     --root_path $root \
-    --output_path $root/colmap/sparse/lidar/points3D.bin \
     --main_lidar_in_transforms lidar_FRONT \
 
 python scripts/pythons/colmap_pts_combine.py \
-    --src1 $root/colmap/sparse/lidar/points3D.bin \
+    --src1 $root/colmap/sparse/lidar/points3D.txt \
     --src2 $root/colmap/sparse/0/points3D.bin \
     --dst $root/colmap/sparse/0/points3D_withlidar.bin
