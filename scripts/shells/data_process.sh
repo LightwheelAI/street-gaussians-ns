2fab0de25498c231d7aee1e68d0bf8364327319f
LightwheelAI <159144751+LightwheelAI@users.noreply.github.com>
2024-05-28 10:56:53 +0800

    FIX sh file error

--- scripts/shells/data_process.sh
+++ scripts/shells/data_process.sh
@@ -8,4 +8,4 @@ sh scripts/shells/run_colmap.sh $root
 
 sh scripts/shells/points_cloud_generate.sh $root
 
-sh scripts/shells/extract_object_pts.py $root
+sh scripts/shells/object_pts_generate.sh $root
